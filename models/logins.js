6aab297f60e6e1be4728c6d9c19a3b9f1fa89613
virtuozogit <125036415+virtuozogit@users.noreply.github.com>
2024-02-22 11:36:59 +0800

    added the initial code and for the server

---
+++ models/logins.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+const loginSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        lowercase: true
+    },
+    logged: {
+        type: Boolean,
+        default: false
+    },
+    rTime: {
+        type: Date
+    },
+    remember: {
+        type: Boolean,
+        default: false
+    },
+    pageNum: {
+        type: Number,
+        default: 0
+    },
+    restoQuery: {
+        type: String
+    }
+})
+
+module.exports = mongoose.model("login", loginSchema)
